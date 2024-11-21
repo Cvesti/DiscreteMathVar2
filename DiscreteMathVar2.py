1e665301f492315c35dd2bcf1a6b4ea3a1badde9
Cvesti <crazivan8@gmail.com>
2024-11-21 19:40:59 +0300

    Updt

--- DiscreteMathVar2.py
+++ DiscreteMathVar2.py
@@ -1,11 +1,27 @@
 def generate_power_set(s):
-
+    """Генерация булеана (всех подмножеств) заданного множества."""
+    
     power_set = []
     n = len(s)
-    for i in range(2**n):
+    
+    for i in range(2 ** n):
         subset = []
         for j in range(n):
             if (i & (1 << j)):
                 subset.append(s[j])
         power_set.append(subset)
+    
     return power_set
+
+
+def main():
+    INPUT_PROMPT = "Введите элементы множества через запятую: "
+    input_set = input(INPUT_PROMPT).split(",")
+    input_set = [item.strip() for item in input_set] 
+    power_set = generate_power_set(input_set)
+
+    print("Булеан заданного множества:", power_set)
+
+
+if __name__ == "__main__":
+    main()
